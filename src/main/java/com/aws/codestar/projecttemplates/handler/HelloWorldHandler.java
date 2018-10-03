1c8a84afb8ce882ff77da227fcd31fe670933933
kiran <kirankumar_reddykotla@intuit.com>
2018-10-03 11:03:47 +0530

    test

--- src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
@@ -17,6 +17,9 @@ public class HelloWorldHandler implements RequestHandler<Object, Object> {
     public Object handleRequest(final Object input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
-        return new GatewayResponse(new JSONObject().put("Output", "Hello World!").toString(), headers, 200);
+        LambdaTest lambdaTest=new LambdaTest();
+        lambdaTest.setName("kiran");
+        lambdaTest.setValue("reddy");
+        return new GatewayResponse(new JSONObject().put("Output",lambdaTest).toString(), headers, 200);
     }
 }
